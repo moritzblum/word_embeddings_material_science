d84c4500af3a0c698a9a9412ee3c294475fa9935
moritzblum <blum-moritz@t-online.de>
2021-11-26 12:58:19 +0100

    reimplemented training pipeline by modularizing the preprocessing, implemented TSNE visualization

--- main.py
+++ main.py
@@ -1,56 +1,63 @@
+import json
 from os import listdir
 import os.path as osp
+from gensim.models import Word2Vec
 from chemdataextractor import Document
 from chemdataextractor.reader import PdfReader
-from gensim.models.callbacks import CallbackAny2Vec
-from gensim.models import Word2Vec
+import matplotlib.pyplot as plt
+from sklearn.manifold import TSNE
 
 
-class MaterialScienceCorpus:
-    def __init__(self, path):
-        """Iterates over sentences form a set of papers."""
-        self.path = path
+def tsne_plot(model):
+    labels = list(model.wv.index_to_key)
+    vectors = [model.wv[label] for label in labels]
 
-    def __iter__(self):
-        self.files = list(filter(lambda x: not x.startswith('.'), listdir(self.path)))
-        self.current_file = []
-        self.current_sentences = []
-        return self
+    tsne_model = TSNE(perplexity=40, n_components=2, init='pca', n_iter=2500, random_state=23)
+    new_values = tsne_model.fit_transform(vectors)
 
-    def __next__(self):
-        # stop iteration if all data is consumed
-        if len(self.current_sentences) == len(self.files) == 0:
-            raise StopIteration
+    x, y = new_values[:, 0], new_values[:, 1]
 
-        # load all data form one file if all sentences are consumed
-        while len(self.current_sentences) == 0:
-
-            # load new paper
-            self.current_file = osp.join(self.path, self.files.pop(0))
-            f = open(self.current_file, 'rb')
-            doc = Document.from_file(f, readers=[PdfReader()])
-            for element in doc.elements:
-                element_tokens = element.tokens
-                for sentence_tokens in element_tokens:
-                    tokens = [token.text for token in sentence_tokens]
-                    self.current_sentences.append(tokens)
-
-        return self.current_sentences.pop(0)
+    plt.figure()
+    for i in range(len(x)):
+        plt.scatter(x[i], y[i])
+        plt.annotate(labels[i],
+                     xy=(x[i], y[i]),
+                     xytext=(5, 2),
+                     textcoords='offset points',
+                     ha='right',
+                     va='bottom')
+    plt.show()
 
 
 if __name__ == '__main__':
-    class callback(CallbackAny2Vec):
-        '''Callback to print loss after each epoch.'''
 
-        def __init__(self):
-            self.epoch = 0
+    preprocess = False
 
-        def on_epoch_end(self, model):
-            loss = model.get_latest_training_loss()
-            print('Loss after epoch {}: {}'.format(self.epoch, loss))
-            self.epoch += 1
+    if preprocess:
+        pdf_dir = './data/pdf'
+        sentences = []
+        files = list(filter(lambda x: not x.startswith('.'), listdir(pdf_dir)))
+        for file in files:
+            f = open(osp.join(pdf_dir, file), 'rb')
+            doc = Document.from_file(f, readers=[PdfReader()])
+            for element in doc.elements:
+                current_sentences = []
+                element_tokens = element.tokens
+                for sentence_tokens in element_tokens:
+                    tokens = [token.text for token in sentence_tokens]
+                    sentences.append(tokens)
 
-    dir = './data'
-    model = Word2Vec(sentences=MaterialScienceCorpus(dir), vector_size=100, window=5, min_count=1, workers=4,
-                     compute_loss=True, callbacks=[callback()])
+        with open('./data/tokenized.json', 'w') as sentences_out:
+            json.dump(sentences, sentences_out)
+    else:
+        with open('./data/tokenized.json') as sentences_in:
+            sentences = json.load(sentences_in)
+
+
+
+    model = Word2Vec(sentences=sentences, vector_size=100, window=5, min_count=20, workers=4,
+                     compute_loss=True)
     model.save("word2vec.model")
+
+    #model.most_similar('trump')
+    tsne_plot(model)
